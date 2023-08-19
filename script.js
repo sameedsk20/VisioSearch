dcb5112a955258d2ad5550d4dc9a74fc63012d87
sameedsk20 <138784854+sameedsk20@users.noreply.github.com>
2023-08-19 14:07:11 +0500

    Image Search Engine
    This search engine is based on HTML, CSS and JS and do the job of searching multiple images of any keyword entered.

---
+++ script.js
@@ -0,0 +1,50 @@
+const searchForm = document.getElementById("searchForm");
+const searchBox = document.getElementById("searchBox");
+const searchResult = document.getElementById("searchResult");
+const showMoreButton = document.getElementById("showMoreButton");
+const accessKey = "62ygmv2uYrc_kh44oSTPNiUoDZnB7SLRDY8wGGSMJ-4";
+
+
+let keyword = "" ;
+let page = 1 ;
+
+async function searchImages(){
+
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if(page === 1){
+        searchResult.innerHTML = "";
+    }
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+
+        imageLink.target="_blank"; // to display image in new tab
+    
+        imageLink.appendChild(image); //the image willl be inside "a" tag
+    
+        searchResult.appendChild(imageLink); //immageLink will be displayed in search result
+    })
+
+    //to display showMore
+    showMoreButton.style.display = "block";
+}
+
+searchForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
+
+showMoreButton.addEventListener("click" , () => {
+    page++
+    searchImages();
+} )
